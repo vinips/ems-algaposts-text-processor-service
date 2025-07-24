646ad0fc99340791d83f75caa15f097588cdeaef
Vini Silveira <viniciusps_ph@hotmail.com>
2025-07-24 19:41:31 -0300

    Configuração inicial do projeto.

---
+++ src/main/java/com/algaworks/algaposts/text/processor/service/TextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaposts.text.processor.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algaposts/text/processor/service/TextProcessorServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.algaworks.algaposts.text.processor.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//Primeiro Commit
+	}
+
+}
